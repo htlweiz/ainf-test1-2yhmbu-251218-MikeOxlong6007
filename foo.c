c9f62b44becc5263aecee4c8777a73d68997aed9
MikeOxlong6007 <cserjes.adam2009@gmail.com>
2025-12-18 12:08:11 +0000

    Keks

--- foo.c
+++ foo.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    printf("%s\n", "Hallo Welt!");
+    //Aufgabe 1:
+    int number_biscuits=0;
+    float diameter_biscuit=0;
+    float choco_area=0;
+    printf("Geben Sie die Anzahl der Kekse ein:");
+    scanf("%d\n",&number_biscuits);
+    printf("Geben Sie den Durchmesser vom Keks ein:");
+    scanf("%f\n",diameter_biscuit);
+    printf("Für die Glasur aller %d Kekse wird Schokolade für %fcm^2 benötigt.");
+    
+
 }
